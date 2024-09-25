982155df178e4cf55f033b76ea3907f010994130
avanishsr <147104174+avanishsr@users.noreply.github.com>
2024-09-25 16:03:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,5 +55,6 @@ def detect_number_plate():
     else:
         return jsonify({"error": "Could not detect number plate"}), 400
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
